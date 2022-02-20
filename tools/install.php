74678d44b5cd4f9990f6c39d872dce1c13734705
alex <766f6964+github@gmail.com>
2022-02-20 20:57:41 +0100

    added install script

---
+++ tools/install.php
@@ -0,0 +1,27 @@
+<?php
+    require ("../vendor/autoload.php");
+
+    unlink(__DIR__ . "/../data/amazon-price-watcher.sqlite3");
+
+    $dbh = new \PDO
+    (
+        sprintf("sqlite:%s", __DIR__ . "/../data/amazon-price-watcher.sqlite3"),
+        null,
+        null,
+        array(
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+        )
+    );
+
+    $queries = array(
+        'CREATE TABLE "GROUPS" ( "ID" TEXT NOT NULL, "NAME" TEXT NOT NULL, PRIMARY KEY("ID") )',
+        'CREATE TABLE "PRODUCT" ( "ID" TEXT NOT NULL, "URL" TEXT NOT NULL UNIQUE, "ASIN" TEXT NOT NULL, "NAME" TEXT NOT NULL, "IMAGE_URL" TEXT, "CURRENT_PRICE" REAL DEFAULT 0, "PREVIOUS_PRICE" REAL DEFAULT 0, "MIN_PRICE" REAL DEFAULT 0, "MAX_PRICE" REAL DEFAULT 0, "CURRENCY" TEXT NOT NULL, "CURRENT_STOCK" TEXT, "LAST_SCRAP" TEXT NOT NULL, PRIMARY KEY("ID") )',
+        'CREATE TABLE "PRODUCT_GROUP" ( "PRODUCT_ID" TEXT NOT NULL, "GROUP_ID" TEXT NOT NULL, PRIMARY KEY("PRODUCT_ID","GROUP_ID") )'
+    );
+
+    foreach($queries as $query) {
+        $stmt = $dbh->prepare($query);
+        $stmt->execute();
+    }
+
+?>
