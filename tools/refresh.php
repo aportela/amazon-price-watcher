114e04b6114d2ea7ad77b56e0f2a0f5977e3f0e4
alex <766f6964+github@gmail.com>
2022-02-22 19:15:01 +0100

    added refresh script

---
+++ tools/refresh.php
@@ -0,0 +1,24 @@
+<?php
+    require ("../vendor/autoload.php");
+
+    $products = \AmazonPriceWatcher\AmazonPriceWatcherItem::search();
+    if (count($products) > 0) {
+        echo "Refreshing products..." . PHP_EOL;
+        foreach($products as $product) {
+            echo "\t[I] Product: " . $product->name;
+            $item = new \AmazonPriceWatcher\AmazonPriceWatcherItem();
+            $item->id = $product->id;
+            $item->get();
+            $item->scrap();
+            if (! empty($item->name)) {
+                $item->update();
+                echo " OK!" . PHP_EOL;
+            } else {
+                echo " ERROR!" . PHP_EOL;
+            }
+            sleep(5);
+        }
+    } else {
+        echo "No products found" . PHP_EOL;
+    }
+?>
